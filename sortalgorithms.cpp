4fb7c223c6f8a040c2f24dc747b234a1dddbe41f
Jia Li Labyrinth <jiali.labyrinth@gmail.com>
2015-02-15 14:19:16 +0800

    Set up preparation to call different sort algorithms

--- sortalgorithms.cpp
+++ sortalgorithms.cpp
@@ -8,18 +8,65 @@
 
 //#include <random>
 //#include <chrono>
-//using namespace std;
+#include <string>
+#include <sstream>
+using namespace std;
 
 SortAlgorithms::SortAlgorithms(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::SortAlgorithms)
 {
     ui->setupUi(this);
+
+    m_btnGrpSortMethod.addButton( ui->rdBtn_BubbleSort,     SORT_BUBBLE );
+    m_btnGrpSortMethod.addButton( ui->rdBtn_HeapSort,       SORT_HEAP );
+    m_btnGrpSortMethod.addButton( ui->rdBtn_InsertionSort,  SORT_INSERT );
+    m_btnGrpSortMethod.addButton( ui->rdBtn_MergeSort,      SORT_MERGE );
+    m_btnGrpSortMethod.addButton( ui->rdBtn_QuickSort,      SORT_QUICK );
+    m_btnGrpSortMethod.addButton( ui->rdBtn_SelectSort,     SORT_SELECTION );
+    m_btnGrpSortMethod.setExclusive( true );
 }
 
 SortAlgorithms::~SortAlgorithms()
 {
     delete ui;
+}
+
+void SortAlgorithms::appendVectorContentToFootprint( const std::vector< int > kVecContent, const QString& strLabel )
+{
+    QString strDispContent;
+    std::ostringstream  os;
+
+    for( size_t i = 0; i < kVecContent.size(); i++ )
+    {
+        os.width( 3 );
+        os.fill( ' ' );
+        os << std::left << kVecContent[ i ];
+    }
+
+    if( strLabel.length() > 0 )
+    {
+        strDispContent = strLabel;
+        strDispContent += " ";
+    }
+    strDispContent += QString::fromStdString( os.str() );
+
+    appendToFootprintOutput( strDispContent );
+}
+
+void SortAlgorithms::appendToFootprintOutput( const QString& strDisplayTxt )
+{
+    ui->txEd_SortFootprint->append( QString(">> ") + strDisplayTxt );
+}
+
+void SortAlgorithms::clearFootprint()
+{
+    ui->txEd_SortFootprint->clear();
+}
+
+void SortAlgorithms::on_btn_ClearFootprint_clicked()
+{
+    clearFootprint();
 }
 
 void SortAlgorithms::on_btn_Randomize_clicked()
@@ -32,11 +79,7 @@ void SortAlgorithms::on_btn_Randomize_clicked()
 //    uniform_int_distribution< int > kRandDistr(0, 60);
 
     // Ask for number of entries
-    iNumOfEntries = QInputDialog::getInt( this,
-                                          "Randomize Raw Input",
-                                          "Please enter total number of inputs",
-                                          10,
-                                          0 );
+    iNumOfEntries = ui->spBx_NumOfEntry->value();
 
     if( iNumOfEntries <= 0 )
     {
@@ -55,5 +98,30 @@ void SortAlgorithms::on_btn_Randomize_clicked()
         QListWidgetItem* pkNewItem = new QListWidgetItem( QString::number( iNewEntryVal ),
                                                           ui->listW_RawInput );
         pkNewItem->setData( Qt::UserRole, kData );
+    }
+}
+
+void SortAlgorithms::on_btn_DoSort_clicked()
+{
+    ESortAlgorithm      eCurrentMethod = static_cast< ESortAlgorithm >( m_btnGrpSortMethod.checkedId() );
+    std::vector< int >  kRawInput;
+    int                 iEntryVal = -1;
+
+    // Grab values from UI
+    kRawInput.reserve( ui->listW_RawInput->count() );
+    for( int i = 0; i < ui->listW_RawInput->count(); i++ )
+    {
+        iEntryVal = ui->listW_RawInput->item( i )->data( Qt::UserRole ).toInt();
+        kRawInput.push_back( iEntryVal );
+    }
+    appendVectorContentToFootprint( kRawInput, "Start to sort input:" );
+
+    // Perform sort
+    switch (eCurrentMethod) {
+    case SORT_INSERT:
+
+        break;
+    default:
+        break;
     }
 }
--- sortalgorithms.h
+++ sortalgorithms.h
@@ -2,10 +2,25 @@
 #define SORTALGORITHMS_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QButtonGroup>
+
+#include <vector>
 
 namespace Ui {
 class SortAlgorithms;
 }
+
+enum ESortAlgorithm
+{
+    SORT_INSERT,
+    SORT_MERGE,
+    SORT_QUICK,
+    SORT_HEAP,
+    SORT_SELECTION,
+    SORT_BUBBLE,
+    SORT_END
+};
 
 class SortAlgorithms : public QMainWindow
 {
@@ -18,11 +33,20 @@ public:
 private:
     int getRandomEntryValue();
 
+    void appendVectorContentToFootprint( const std::vector< int > kVecContent, const QString& strLabel = "" );
+
+    void appendToFootprintOutput( const QString& strDisplayTxt );
+    void clearFootprint();
+
 private slots:
+    void on_btn_ClearFootprint_clicked();
+
     void on_btn_Randomize_clicked();
+    void on_btn_DoSort_clicked();
 
 private:
-    Ui::SortAlgorithms *ui;
+    Ui::SortAlgorithms* ui;
+    QButtonGroup        m_btnGrpSortMethod;
 };
 
 #endif // SORTALGORITHMS_H
